b6e8c580e8f305cc98ced9a0a336d8086ac2a8f7
codervashu <148979887+codervashu@users.noreply.github.com>
2023-11-26 22:41:52 +0530

    Add files via upload

---
+++ Own web/js/script.js
@@ -0,0 +1,13 @@
+menu  = document.querySelector('.menu')
+navbar = document.querySelector('.v-class')
+navlist = document.querySelector('.h-nav')
+
+menu.addEventListener('click',()=>{
+    navbar.classList.toggle('v-class');
+    navlist.classList.toggle('h-nav');
+})
+
+console.log('this is vashu') 
+
+
+
